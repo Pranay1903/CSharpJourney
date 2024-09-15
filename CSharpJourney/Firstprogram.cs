32bd5e9d5d26e8e8e0140965ee5ea8e52c1524ef
Pranay Patel <pranaypatel1933@gmail.com>
2024-09-15 23:25:10 +0530

    Explored Datatypes

---
+++ CSharpJourney/Firstprogram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpJourney
+{
+    class Firstprogram
+    {
+        static void Main(string[] args)
+        {
+            string name = "pranay";
+            
+            Console.WriteLine(name + " " + (15 + 15) );
+
+            Console.WriteLine("First Program");
+        }
+    }
+}
---
+++ CSharpJourney/datatypes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpJourney
+{
+    internal class datatypes
+    {
+
+        public datatypes() {
+
+            //integer size-4 bytes 
+            int num = 10;
+            Console.WriteLine(num);
+
+            //long size-8 bytes
+            long numl = 15000000000L;
+            Console.WriteLine(numl);
+
+            //float size-4 bytes
+            float numf = 5.75F;
+            Console.WriteLine(numf);
+
+            //double size-8 bytes
+            double numd = 
+
+
+        }
+
+    }
+}
